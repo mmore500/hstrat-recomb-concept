9aa96fa7d2e8a626463aa1ca07026acb50dfd27f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-05-22 21:49:53 -0700

    Revert "Fix potential copy bug causing asynchronization"
    This reverts commit d7c2319cc8ba13f0a98333d352ee242b52389e47.

--- pylib/deap/hstrat_mate_decorator.py
+++ pylib/deap/hstrat_mate_decorator.py
@@ -1,4 +1,3 @@
-import copy
 import random
 
 from ..hstrat import recombine_strategy_stratum_genedrive
@@ -7,8 +6,6 @@ from ..hstrat import recombine_strategy_stratum_genedrive
 def hstrat_mate_decorator(func):
     def wrapFunc(*args, **kwargs):
         i1, i2 = func(*args, **kwargs)
-        i1 = copy.deepcopy(i1)
-        i2 = copy.deepcopy(i2)
         child_annotation = recombine_strategy_stratum_genedrive(
             i1.species_annotation,
             i2.species_annotation,
